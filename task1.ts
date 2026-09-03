75b1165bcaf8ea98500c76f7e1cb827699744f59
ivserada <ivserada@gmail.com>
2026-09-03 20:52:45 +0300

    feat: task1 resolution

--- task1.ts
+++ task1.ts
@@ -1,8 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const name: string = "Александр";
 
 const text: string = `Я, ${name}, учу TYPESCRIPT`;
 
 //Написать программу, которая выводит text в нижнем регистре с вашим именем, если имя не совпадает, то заменить его
 
-const yourName: string = "Настя";
+const yourName: string = "Иван";
+if (text.includes(yourName)) {
+  console.log(text.toLowerCase());
+} else {
+  const newText: string = text.slice(0, text.indexOf(name)).concat(yourName.concat(text.slice()));
+  console.log(newText);
+}
